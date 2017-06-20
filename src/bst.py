297583f15145f042d6f402eee3723ad0d4d81e27
James <jjfeore>
2017-06-20 13:48:47 -0700

    Ready for tests

--- src/bst.py
+++ src/bst.py
@@ -14,20 +14,20 @@ class Node(object):
 class BinTree(object):
     """Implement a binary search tree."""
 
-    def __init__(self, val):
+    def __init__(self, val=None):
         """Instantiate a new BST."""
         self._root = None
         self._size = 0
-        if val is None:
-            raise ValueError('Please enter an integer or iterable of integers')
-        elif type(val) in [list, tuple]:
+        if type(val) in [list, tuple]:
             for item in val:
                 self.insert(item)
+        elif not val:
+            raise ValueError('BST only accepts optional parameter or a list or tuple')
 
     def insert(self, val):
         """Insert a value into a BST."""
         if type(val) not in [int, float]:
-            raise ValueError('Please insert only integers')
+            raise ValueError('Please insert only integers or floats')
         if not self._root:
             self._root = Node(val)
             self._size += 1
@@ -76,6 +76,53 @@ class BinTree(object):
                         return None
                 elif val == curr.val:
                     return curr
+
+    def contains(self, val):
+        """Check to see if the BST contains a value."""
+        if self.search(val):
+            return True
+        return False
+
+    def size(self):
+        """Return the total number of nodes in the BST."""
+        return self._size
+
+    def depth(self, node=None):
+        """Find the depth of a specified node."""
+        if not node:
+            node = self._root
+            if not node:
+                return 0
+
+        l_depth = 0
+        r_depth = 0
+
+        if node.left:
+            l_depth = self.depth(node.left)
+        if node.right:
+            r_depth = self.depth(node.right)
+
+        if not node.left and not node.right:
+            return 1
+        elif l_depth >= r_depth:
+            return l_depth + 1
+        elif l_depth < r_depth:
+            return r_depth + 1
+
+    def balance(self):
+        """Return an integer representing if the tree is balanced or not."""
+        start = self._root
+        if not start:
+            return 0
+
+        l_depth = 0
+        r_depth = 0
+        if start.left:
+            l_depth = self.depth(start.left)
+        if start.right:
+            r_depth = self.depth(start.right)
+
+        return l_depth - r_depth
 
 
 if __name__ == '__main__':
--- src/test_bst.py
+++ src/test_bst.py
@@ -6,87 +6,81 @@ import pytest
 def binary_tree():
     """Binary tree for testing."""
     from bst import BinTree
-    new_tree = BinTree(5)
+    new_tree = BinTree()
+    new_tree.insert(5)
     return new_tree
 
 
 @pytest.fixture
-def bigger_binary_tree(binary_tree):
+def bigger_binary_tree():
     """Larger binary tree for testing."""
     from bst import BinTree
-    binary_tree.insert(8)
-    binary_tree.insert(2)
-    binary_tree.insert(3)
-    binary_tree.insert(1)
-    binary_tree.insert(7)
-    binary_tree.insert(9)
-    return binary_tree
+    new_tree = BinTree([8, 2, 3, 1, 7, 9])
+    return new_tree
 
 
-def test_init_with_val():
-    """."""
+def test_init_with_empty_bst():
+    """Test to see if an empty BST has a root or size."""
     from bst import BinTree
-    new_tree = BinTree(5)
-    assert new_tree.val == 5
-    assert new_tree.left is None
-    assert new_tree.right is None
+    new_tree = BinTree()
+    assert new_tree._root is None
+    assert new_tree._size == 0
+
+
+def test_init_with_val(binary_tree):
+    """Test to see if a single depth BST has a root."""
+    assert binary_tree._root.val == 5
+    assert binary_tree._root.left is None
+    assert binary_tree._root.right is None
+    assert binary_tree._size == 1
 
 
 def test_init_with_iter():
-    """."""
+    """Test to see if instantiating with an iterable works."""
     from bst import BinTree
     new_tree = BinTree([3, 2, 1, 4, 5])
-    assert new_tree.val == 3
-    assert new_tree.left.val == 2
-    assert new_tree.right.val == 4
-    assert new_tree.left.left.val == 1
-    assert new_tree.right.right.val == 5
+    assert new_tree._root.val == 3
+    assert new_tree._root.left.val == 2
+    assert new_tree._root.right.val == 4
+    assert new_tree._root.left.left.val == 1
+    assert new_tree._root.right.right.val == 5
 
 
-def test_init_with_non_int():
-    """Must accept int or list of ints for now."""
+def test_insert_with_non_int():
+    """Test that it only accepts a list or tuple as the optional param."""
     from bst import BinTree
     with pytest.raises(ValueError):
-        new_tree = BinTree("new tree")
-
-
-def test_init_with_none():
-    """Cannot be initialized with nothing."""
-    from bst import BinTree
-    new_tree = BinTree()
-    new_tree.val is None
-    new_tree.left is None
-    new_tree.right is None
+        BinTree('this should break it')
 
 
 def test_insert_smaller(binary_tree):
     """Test insertion success."""
     binary_tree.insert(4)
-    assert binary_tree.left.val == 4
-    assert binary_tree.right.val is None
+    assert binary_tree._root.left.val == 4
+    assert binary_tree._root.right is None
 
 
 def test_insert_larger(binary_tree):
     """Test insertion success."""
     binary_tree.insert(6)
-    assert binary_tree.left.val is None
-    assert binary_tree.right.val == 6
+    assert binary_tree._root.left is None
+    assert binary_tree._root.right.val == 6
 
 
 def test_insert_smaller_isolated(binary_tree):
     """Test insertion with a left node already there."""
     binary_tree.insert(4)
     binary_tree.insert(6)
-    assert binary_tree.left.val == 4
-    assert binary_tree.right.val == 6
+    assert binary_tree._root.left.val == 4
+    assert binary_tree._root.right.val == 6
 
 
 def test_insert_larger_isolated(binary_tree):
     """Test insertion with a right node already there."""
     binary_tree.insert(6)
     binary_tree.insert(4)
-    assert binary_tree.left.val == 4
-    assert binary_tree.right.val == 6
+    assert binary_tree._root.left.val == 4
+    assert binary_tree._root.right.val == 6
 
 
 def test_insert_depth(binary_tree):
@@ -97,16 +91,22 @@ def test_insert_depth(binary_tree):
     binary_tree.insert(1)
     binary_tree.insert(7)
     binary_tree.insert(9)
-    assert binary_tree.left.left.val == 1
-    assert binary_tree.left.right.val == 3
-    assert binary_tree.right.left.val == 7
-    assert binary_tree.right.right.val == 9
+    assert binary_tree._root.left.left.val == 1
+    assert binary_tree._root.left.right.val == 3
+    assert binary_tree._root.right.left.val == 7
+    assert binary_tree._root.right.right.val == 9
 
 
 def test_no_inserting_non_numbers(binary_tree):
-    """."""
+    """Test to see if the insert method only accepts numbers."""
     with pytest.raises(ValueError):
         binary_tree.insert('a tree')
+
+
+def test_search_with_non_number(binary_tree):
+    """Test to see if searching for a string returns an error."""
+    with pytest.raises(ValueError):
+        binary_tree.search('a tree')
 
 
 def test_search(binary_tree):
@@ -114,7 +114,8 @@ def test_search(binary_tree):
 
     Not really sure what we're actually returning here.
     """
-    assert isinstance(binary_tree.search(5), BinTree)
+    from bst import Node
+    assert isinstance(binary_tree.search(5), Node)
     assert binary_tree.search(0) is None
 
 
@@ -182,12 +183,6 @@ def test_contains(bigger_binary_tree):
     bigger_binary_tree.contains(8) is True
     bigger_binary_tree.contains(3) is True
     bigger_binary_tree.contains(17) is False
-
-
-def test_contains_given_none_raises_exception(bigger_binary_tree):
-    """Test error raised."""
-    with pytest.raises(ValueError):
-        bigger_binary_tree.contains()
 
 
 def test_balance(bigger_binary_tree, binary_tree):
