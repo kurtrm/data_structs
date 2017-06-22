873e3dbde028b0d6e23682d0309cefc050dd9c19
Kurt <kurtrm@gmail.com>
2017-06-21 22:21:08 -0700

    Issues with tests. Exploring failures.

--- src/test_traversals.py
+++ src/test_traversals.py
@@ -32,28 +32,31 @@ def small_bst():
 
 # ===== In Order Traversals =====
 
+#  Left off wondering why test with no arguments is equal to self.root
 
-def test_only_accepts_number(small_bst):
+# Also, exploring post and pre order test failures
+
+def test_in_order_only_accepts_number(small_bst):
     """Ensure each test only accepts an integer or float."""
     with pytest.raises(TypeError):
         small_bst.in_order('stringy')
 
 
-def test_node_must_be_in_binary_tree_to_start_traversal(binary_tree):
+def test_in_order_node_must_be_in_binary_tree_to_start_traversal(binary_tree):
     """Ensure a node not in the tree can't be passed into the method."""
     with pytest.raises(ValueError):
         binary_tree.in_order(1000)
 
 
-def test_yields_none_if_empty(empty_bst):
+def test_in_order_yields_none_if_empty(empty_bst):
     """Ensure we get None if empty."""
     assert next(empty_bst.in_order()) is None
 
 
 def test_starts_at_root_with_no_arguments(binary_tree):
-    """starts at root."""
+    """Ensure starts at root."""
     generator = binary_tree.in_order()
-    assert next(generator) == binary_tree._root
+    assert next(generator) == binary_tree._root.val
 
 
 def test_yields_root_if_no_left_and_right(empty_bst):
@@ -110,12 +113,176 @@ def test_small(small_bst):
 # ===== Pre-Order Traversal =====
 
 
+def test_pre_order_only_accepts_number(small_bst):
+    """Ensure each test only accepts an integer or float."""
+    with pytest.raises(TypeError):
+        small_bst.pre_order('stringy')
 
+
+def test_pre_order_node_must_be_in_binary_tree_to_start_traversal(binary_tree):
+    """Ensure a node not in the tree can't be passed into the method."""
+    with pytest.raises(ValueError):
+        binary_tree.pre_order(1000)
+
+
+def test_pre_order_yields_none_if_empty(empty_bst):
+    """Ensure we get None if empty."""
+    assert next(empty_bst.pre_order()) is None
+
+
+def test_pre_order_starts_at_root_with_no_arguments(binary_tree):
+    """starts at root."""
+    generator = binary_tree.pre_order()
+    assert next(generator) == binary_tree._root.val
+
+
+def test_pre_order_yields_root_if_no_left_and_right(empty_bst):
+    """."""
+    empty_bst.insert(5)
+    generator = empty_bst.pre_order()
+    assert next(generator) == 5
+
+
+def test_pre_order_yields_left_nodes(empty_bst):
+    """."""
+    empty_bst.insert(5)
+    empty_bst.insert(4)
+    empty_bst.insert(3)
+    empty_bst.insert(2)
+    empty_bst.insert(1)
+    empty_bst.insert(0)
+    generator = empty_bst.pre_order()
+    assert list(generator) == [5, 4, 3, 2, 1, 0]
+
+
+def test_pre_order_yields_right_nodes(empty_bst):
+    """."""
+    empty_bst.insert(5)
+    empty_bst.insert(6)
+    empty_bst.insert(7)
+    empty_bst.insert(8)
+    empty_bst.insert(9)
+    empty_bst.insert(10)
+    generator = empty_bst.pre_order()
+    assert list(generator) == [5, 6, 7, 8, 9, 10]
+
+
+def test_pre_order_v_shaped_bst():
+    """Test odd shape."""
+    from bst import BinTree
+    beast = BinTree([5, 4, 3, 2, 1, 6, 7, 8, 9, 10])
+    generator = beast.pre_order()
+    assert list(generator) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_pre_order_big():
+    """."""
+    from bst import BinTree
+    beast = BinTree([45, 33, 1, 8, 93, 10, 63, 44, 100, 4, 79, 69, 7, 54, 0, 16, 94, 14, 49, 11])
+    generator = beast.pre_order()
+    assert list(generator) == [45, 33, 1, 0, 8, 4, 7, 10, 16, 14, 11, 44, 93, 63, 54, 49, 79, 69, 100, 94]
+
+
+def test_pre_order_small():
+    """."""
+    from bst import BinTree
+    beast = BinTree([10, 5, 11, 13, 4, 6, 12, 20, 21, 7])
+    generator = beast.pre_order()
+    assert list(generator) == [10, 5, 4, 6, 7, 11, 13, 12, 20, 21]
 
 
 # ===== Post-Order Traversal =====
 
 
+def test_post_order_only_accepts_number(small_bst):
+    """Ensure each test only accepts an integer or float."""
+    with pytest.raises(TypeError):
+        small_bst.post_order('stringy')
+
+
+def test_post_order_node_must_be_in_binary_tree_to_start_traversal(binary_tree):
+    """Ensure a node not in the tree can't be passed into the method."""
+    with pytest.raises(ValueError):
+        binary_tree.post_order(1000)
+
+
+def test_post_order_yields_none_if_empty(empty_bst):
+    """Ensure we get None if empty."""
+    assert next(empty_bst.post_order()) is None
+
+
+def test_post_order_starts_at_root_with_no_arguments(binary_tree):
+    """Ensure starts at root."""
+    generator = binary_tree.post_order()
+    assert next(generator) == binary_tree._root.val
+
+
+def test_post_order_yields_root_if_no_left_and_right(empty_bst):
+    """."""
+    empty_bst.insert(5)
+    generator = empty_bst.post_order()
+    assert next(generator) == 5
+
+
+def test_post_order_yields_left_nodes(empty_bst):
+    """."""
+    empty_bst.insert(5)
+    empty_bst.insert(4)
+    empty_bst.insert(3)
+    empty_bst.insert(2)
+    empty_bst.insert(1)
+    empty_bst.insert(0)
+    generator = empty_bst.post_order()
+    assert list(generator) == [0, 1, 2, 3, 4, 5]
+
+
+def test_post_order_yields_right_nodes(empty_bst):
+    """."""
+    empty_bst.insert(5)
+    empty_bst.insert(6)
+    empty_bst.insert(7)
+    empty_bst.insert(8)
+    empty_bst.insert(9)
+    empty_bst.insert(10)
+    generator = empty_bst.post_order()
+    assert list(generator) == [10, 9, 8, 7, 6, 5]
+
+
+def test_post_order_v_shaped_bst():
+    """Test odd shape."""
+    from bst import BinTree
+    beast = BinTree([5, 4, 3, 2, 1, 6, 7, 8, 9, 10])
+    generator = beast.post_order()
+    assert list(generator) == [1, 2, 3, 4, 10, 9, 8, 7, 6, 5]
+
+
+def test_post_order_big():
+    """."""
+    from bst import BinTree
+    beast = BinTree([45, 33, 1, 8, 93, 10, 63, 44, 100, 4, 79, 69, 7, 54, 0, 16, 94, 14, 49, 11])
+    generator = beast.post_order()
+    assert list(generator) == [0, 7, 4, 11, 14, 16, 10, 8, 1, 44, 33, 49, 54, 69, 79, 63, 94, 100, 93, 45]
+
+
+def test_post_order_small():
+    """."""
+    from bst import BinTree
+    beast = BinTree([10, 5, 11, 13, 4, 6, 12, 20, 21, 7])
+    generator = beast.post_order()
+    assert list(generator) == [4, 7, 6, 5, 12, 21, 20, 13, 11, 10]
+
+
 
 # ===== Breadth-First Traversal =====
 
+
+def test_breadth_yields_none_if_empty(empty_bst):
+    """Ensure we get None if empty."""
+    assert next(empty_bst.breadth_first()) is None
+
+
+def test_breadth_yields_root_if_no_left_and_right(empty_bst):
+    """."""
+    empty_bst.insert(5)
+    generator = empty_bst.breadth_first()
+    assert next(generator) == 5
