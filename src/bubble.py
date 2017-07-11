34804ea38ef99596d0621d9b4b36f1287adcad4b
James <jjfeore>
2017-07-11 11:46:27 -0700

    Added timeit

--- src/bubble.py
+++ src/bubble.py
@@ -17,3 +17,19 @@ def bubble_sort(iter):
                 iter[idx + 1] = iter[idx]
                 iter[idx] = tmp
     return iter
+
+
+if __name__ == '__main__':  # pragma: no cover
+    from timeit import Timer
+    random = Timer(
+        'bubble_sort([randint(0, 1000) for x in range(100)][::-1])',
+        "from __main__ import bubble_sort; from random import randint"
+    )
+    print("""
+Bubble sort, sometimes referred to as sinking sort, is a simple
+sorting algorithm that repeatedly steps through the list to be sorted,
+compares each pair of adjacent items and swaps them if they are in the
+wrong order.
+""")
+    print("Random input (100 numbers from 0-1000 sorted) over 1000 trials:")
+    print(random.timeit(number=1000))
