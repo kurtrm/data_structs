37404121f8b5bc15168dfb0c55a618a4afaa56fe
James <jjfeore>
2017-07-11 10:28:23 -0700

    Added error handling

--- src/bubble.py
+++ src/bubble.py
@@ -3,12 +3,15 @@
 
 def bubble_sort(iter):
     """Sort the iterable using the Bubble sort method."""
-    size = 0
-    while size < (len(iter) - 1):
-        size += 1
-        for idx in range(len(iter) - size):
-            if iter[idx] > iter[idx + 1]:
-                tmp = iter[idx + 1]
-                iter[idx + 1] = iter[idx]
-                iter[idx] = tmp
-    return iter
+    try:
+        size = 0
+        while size < (len(iter) - 1):
+            size += 1
+            for idx in range(len(iter) - size):
+                if iter[idx] > iter[idx + 1]:
+                    tmp = iter[idx + 1]
+                    iter[idx + 1] = iter[idx]
+                    iter[idx] = tmp
+        return iter
+    except TypeError:
+        raise TypeError("Input only a list/tuple of integers")
